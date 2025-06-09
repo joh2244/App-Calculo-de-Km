545eb1c3dc945b48e6607e348ed2095b0a8a757d
Josué Silva <89610133+joh2244@users.noreply.github.com>
2025-06-08 21:09:59 -0300

    app
    app

---
+++ index.js
@@ -0,0 +1,31 @@
+//Objetivo: Identificar quantos litros de gasolina preciso para percorrer um determinado trajeto
+
+/*
+do {
+    var km_porLitroCarro = Number(prompt("Consumo por Litro do veículo: "));
+    var quantidade_KmTotal = Number(prompt("Quantos km total da viagem:"));
+    var preco_Gasolina = parseFloat(prompt("Preço da gasolina por Litro:"));
+
+    var quantidade_LitroTotal = quantidade_KmTotal / km_porLitroCarro;
+
+    var total_Gasolina = quantidade_LitroTotal * preco_Gasolina;
+
+    if (km_porLitroCarro == "" || quantidade_KmTotal == "" || preco_Gasolina == "") {
+        alert("Preencha todos os campos");
+    }
+
+
+} while (km_porLitroCarro == "" || quantidade_KmTotal == "" || preco_Gasolina == "")
+
+
+
+
+console.log("Em um trajeto para a praia com Km total de 200, e valor da gasolina 5 reais, descubra quantos litros sao necessarios e o valor total a pagar")
+
+console.log(`Quantidade de Litros total: ${quantidade_LitroTotal} Litros`);
+
+console.log(`Preço da Gasolina:R$  ${preco_Gasolina.toFixed(2)}`);
+
+console.log(`Gasto Total com Gasolina: R$ ${total_Gasolina.toFixed(2)}`);
+
+*/
